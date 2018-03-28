1e793d5d217891e564ad26962b59f99b17e9eb47
NhungNguyen07 <nhungnt.epu@gmail.com>
2018-03-28 11:45:30 +0700

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
-  before_save{ self.email = email.downcase }
-  validates :name, presence: true, length:{ maximum: Settings.name.max_length}
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: Settings.name.max_length}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length:{ maximum: Settings.email.max_length},
-    format:{ with: VALID_EMAIL_REGEX },
-    uniqueness:{ case_sensitive: false }
+  validates :email, presence: true, length: {maximum: Settings.email.max_length},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length:{ minimum: Settings.password.min_length}
+  validates :password, presence: true, length: {minimum: Settings.password.min_length}
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create string, cost: cost
+  end
 end
